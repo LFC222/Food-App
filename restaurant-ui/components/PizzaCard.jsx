e986dcdae3a6afcc84582f40ccfed98f10a5bc1b
Lucas FC <lucasfernandes_222@hotmail.com>
2022-11-16 01:56:35 -0300

    Responsivity

--- restaurant-ui/components/PizzaCard.jsx
+++ restaurant-ui/components/PizzaCard.jsx
@@ -1,18 +1,17 @@
-import Image from 'next/image'
-import React from 'react'
-import styles from "../styles/PizzaCard.module.css"
+import Image from "next/image";
+import styles from "../styles/PizzaCard.module.css";
 
-function PizzaCard() {
+const PizzaCard = () => {
   return (
     <div className={styles.container}>
-        <Image src="/img/pizza.png" alt="" width={200} height={200}/>
-        <h1 className={styles.title}> FIORI DI ZUCCA</h1>
-        <span className={styles.price}>R$40.50</span>
-        <p className={styles.desc}>
-          Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
-        </p>
+      <Image src="/img/pizza.png" alt="" width="300" height="300" />
+      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
+      <span className={styles.price}>$19.90</span>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default PizzaCard
+export default PizzaCard;
--- restaurant-ui/components/PizzaList.jsx
+++ restaurant-ui/components/PizzaList.jsx
@@ -1,31 +1,27 @@
-import styles from "../styles/PizzaList.module.css"
-import PizzaCard from "../components/PizzaCard"
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard"
 
-import React from 'react'
-
-function PizzaList() {
+const PizzaList = () => {
   return (
     <div className={styles.container}>
-        <h1 className={styles.title}>BEST PIZZA IN THE CITY !</h1>
-        <p className={styles.desc}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Integer lacus tortor, dictum ut odio sed, luctus mollis mi. 
-        Fusce dignissim neque sit amet dictum efficitur. 
-        Vestibulum eros quam, vestibulum vel viverra et, pulvinar a nulla. 
-        Aliquam erat volutpat.
-        </p>
-        <div className={styles.wrapper}>
-            <PizzaCard/>
-            <PizzaCard/>
-            <PizzaCard/>
-            <PizzaCard/>
-            <PizzaCard/>
-            <PizzaCard/>
-            <PizzaCard/>
-            <PizzaCard/>
-        </div>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit arcu
+        in pretium molestie. Interdum et malesuada fames acme. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit.
+      </p>
+      <div className={styles.wrapper}>
+          <PizzaCard/>
+          <PizzaCard/>
+          <PizzaCard/>
+          <PizzaCard/>
+          <PizzaCard/>
+          <PizzaCard/>
+          <PizzaCard/>
+          <PizzaCard/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PizzaList
+export default PizzaList;
