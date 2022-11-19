27c01d710bbd8c04d13c1a1b9f3a69c40ac88458
Lucas FC <lucasfernandes_222@hotmail.com>
2022-11-19 15:10:50 -0300

    cart

--- restaurant-ui/pages/cart.jsx
+++ restaurant-ui/pages/cart.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '../styles/Cart.module.css'
+import {useDispatch,useSelector} from 'react-redux'
 
 const Cart = () => {
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart);
+
+
   return (
     <div className={styles.container}>
         <div className={styles.left}>
@@ -15,73 +20,58 @@ const Cart = () => {
                     <th>Quantity</th>
                     <th>Total</th>
                 </tr>
-                <tr className={styles.tr}>
-                    <td>
-                        <div className={styles.imageContainer}>
-                            <Image 
-                                src="/img/pizza.png" 
-                                alt="" 
-                                layout='fill' 
-                                objectFit='cover'
-                            />
-                        </div>
-                    </td>
-                    <td>
-                        <span className={styles.name}>CORALZO</span>
-                    </td>
-                    <td>
-                        <span className={styles.extras}>Double ingredients, spicy sauce</span>
-                    </td>
-                    <td>
-                        <span className={styles.price}>R$19.90</span>
-                    </td>
-                    <td>
-                        <span className={styles.quantity}>2</span>
-                    </td>
-                    <td>
-                        <span className={styles.total}>R$39.80</span>
-                    </td>
-                </tr>
-                <tr className={styles.tr}>
-                    <td>
-                        <div className={styles.imageContainer}>
-                            <Image 
-                                src="/img/pizza.png" 
-                                alt="" 
-                                layout='fill' 
-                                objectFit='cover'
-                            />
-                        </div>
-                    </td>
-                    <td>
-                        <span className={styles.name}>CORALZO</span>
-                    </td>
-                    <td>
-                        <span className={styles.extras}>Double ingredients, spicy sauce</span>
-                    </td>
-                    <td>
-                        <span className={styles.price}>R$19.90</span>
-                    </td>
-                    <td>
-                        <span className={styles.quantity}>2</span>
-                    </td>
-                    <td>
-                        <span className={styles.total}>R$39.80</span>
-                    </td>
-                </tr>
+                {
+                    cart.products.map((product) => (
+                        <tr className={styles.tr} key={product._id}>
+                            <td>
+                                <div className={styles.imageContainer}>
+                                    <Image 
+                                        src={product.img} 
+                                        alt="" 
+                                        layout='fill' 
+                                        objectFit='cover'
+                                    />
+                                </div>
+                            </td>
+                            <td>
+                                <span className={styles.name}>{product.title}</span>
+                            </td>
+                            <td>
+                                <span className={styles.extras}>
+                                    {
+                                        product.extras.map((extra) => (
+                                            <span key={extra._id}>{extra.text}, </span>
+                                        ))
+                                    }
+                                </span>
+                            </td>
+                            <td>
+                                <span className={styles.price}>R${product.price}</span>
+                            </td>
+                            <td>
+                                <span className={styles.quantity}>{product.quantity}</span>
+                            </td>
+                            <td>
+                                <span className={styles.total}>
+                                    R${product.price * product.quantity}
+                                </span>
+                            </td>
+                        </tr>
+                    ))
+                }
             </table>
         </div>
         <div className={styles.right}>
             <div className={styles.wrapper}>
                 <h2 className={styles.title}>CART TOTAL</h2>
                 <div className={styles.totalText}>
-                    <b className={styles.totalTextTitle}>Subtotal:</b>R$79.60
+                    <b className={styles.totalTextTitle}>Subtotal:</b>R${cart.total}
                 </div>
                 <div className={styles.totalText}>
                     <b className={styles.totalTextTitle}>Discount:</b>R$0.00
                 </div>
                 <div className={styles.totalText}>
-                    <b className={styles.totalTextTitle}>Total:</b>R$79.60
+                    <b className={styles.totalTextTitle}>Total:</b>R${cart.total}
                 </div>
                 <button className={styles.button}>CHECKOUT NOW!</button>
             </div>
